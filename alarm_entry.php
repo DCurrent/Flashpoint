185a6f5e3a5271cd2cfcd1717be1fb2774961c0c
Caskey, Damon V <dc@caskeys.com>
2021-02-23 17:50:05 -0500

    Add date and yime picker to time of incident, time reset, and time silenced fields.

--- alarm_entry.php
+++ alarm_entry.php
@@ -450,11 +450,16 @@
                             	class	="form-control"  
                                 name	="time_reported" 
                                 id		="time_reported"
-                                placeholder	="yyyy-mm-dd hh:mm" 
+                                placeholder	="Date and Time" 
+                                data-role="datebox" 
+                                data-datebox-mode="datetimebox"
+                                data-options='{"useFocus":"true", "displayDropdownPosition":"bottomLeft"}'
                             	value="<?php echo $_main_data->get_time_reported(); ?>"
                                 required>
                         </div>                        
                     </div>
+                    
+                    
                     
                     <div class="form-group row">
                         <label class="col-sm-2" for="time_silenced">Time Silenced</label>
@@ -463,7 +468,10 @@
                             	class	="form-control"  
                                 name	="time_silenced" 
                                 id		="time_silenced"
-                                placeholder	="yyyy-mm-dd hh:mm" 
+                                placeholder	="Date and Time" 
+                                data-role="datebox" 
+                                data-datebox-mode="datetimebox"
+                                data-options='{"useFocus":"true", "displayDropdownPosition":"bottomLeft"}'
                             	value="<?php echo $_main_data->get_time_silenced(); ?>"
                                 required>
                         </div>                        
@@ -476,7 +484,10 @@
                             	class	="form-control"  
                                 name	="time_reset" 
                                 id		="time_reset"
-                                placeholder	="yyyy-mm-dd hh:mm" 
+                                placeholder	="Date and Time" 
+                                data-role="datebox" 
+                                data-datebox-mode="datetimebox"
+                                data-options='{"useFocus":"true", "displayDropdownPosition":"bottomLeft"}'
                             	value="<?php echo $_main_data->get_time_reset(); ?>"
                                 required>
                         </div>                        
@@ -695,8 +706,19 @@
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+    
+        <!-- DateTime Picker (https://datebox.jtsage.dev/) -->)
+    <script src="https://cdn.jsdelivr.net/npm/jtsage-datebox-bootstrap4@5.3.3/jtsage-datebox.min.js" type="text/javascript"></script>
+
         
     <script>
+    $(document).ready(function()
+     {
+
+         $("#dtBox").DateTimePicker();
+
+     });
+        
   $('.room_search').change(function(event)
     {	
 		//options_update(event, null, '#room_code');	
